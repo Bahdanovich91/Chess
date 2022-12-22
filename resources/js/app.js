35bb434ede92fdb1bc33ff0a7d9ffb2db3a28aef
Bahdanovich <Bahdanovich@internet.ru>
2022-12-23 00:12:34 +0300

    Made a chessboard

---
+++ resources/js/app.js
@@ -0,0 +1,28 @@
+var divSquare = '<div id="s$coord" class="square $color"></div>';
+var divFigure = '<div id="f$coord" class="figure">$figure</div>';
+
+$(function () {
+    addSquares();
+    showFigureAt(0, 'r');
+    showFigureAt(63, 'R');
+
+});
+
+function addSquares() {
+    $('.board').html('');
+    for (var coord = 0; coord < 64; coord++) {
+        $('.board').append(divSquare
+            .replace('$coord', coord)
+            .replace('$color', isBlackSquareAt(coord) ? 'black' : 'white'));
+    }
+}
+
+function showFigureAt(coord, figure) {
+    $('#s' + coord).html(divFigure
+        .replace('$coord', coord)
+        .replace('$figure', figure));
+}
+
+function isBlackSquareAt(coord) {
+    return (coord % 8 + Math.floor(coord / 8)) % 2;
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Chess</title>
+    <link rel="stylesheet" href="/css/app.css">
+    <script src="https://code.jquery.com/jquery-3.6.3.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"
+            integrity="sha256-xLD7nhI62fcsEZK2/v8LsBcb4lG7dgULkuXoXB/j91c="
+            crossorigin="anonymous"></script>
+    <script src="/js/app.js"></script>
+</head>
+<body>
+
+<div class="board"></div>
+
+</body>
+</html>
